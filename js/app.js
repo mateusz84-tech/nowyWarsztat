0ffadc63b01d15bf291368daa27a81626b752de0
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-02-23 14:32:33 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+$(function () {
+
+    // // zadanie 1
+    // var currentData = $(".current-date");
+    //
+    // var DATE_API = "http://date.jsontest.com";
+    //
+    // function fetchDate() {
+    //     $.ajax({
+    //         url:DATE_API,
+    //         type: "GET"
+    //     }).done(function (result) {
+    //         console.log(result);
+    //         currentData.text(result.time + ", " + result.date);
+    //     })
+    // }
+    // fetchDate();
+    //
+    // // Zadanie 2
+    // var starWarsContainer = $(".star-wars");
+    // var STARWARS_API = "https://swapi.co/api";
+    //
+    // function fetchStarsWars() {
+    //     $.ajax({
+    //         url: STARWARS_API + "/people/4",
+    //         type: "GET"
+    //     }).done(function (result) {
+    //         renderPerson(result);
+    //     })
+    // }
+    // fetchStarsWars();
+    //
+    // function renderPerson(person) {
+    //     var name = $("<strong>").text(person.name);
+    //     var height = $("<div>").text(person.height + "cm");
+    //
+    //     starWarsContainer.append(name,height);
+    // }
+
+    // ========= Testowanie API GET =============
+
+});
+
